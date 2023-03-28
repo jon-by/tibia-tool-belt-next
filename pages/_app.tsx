ec40d2973ce01d2d4f0fc65c4a411f7bfe00f32d
Jonas Yure <yure.pereira@codeby.com.br>
2023-03-28 08:11:48 -0300

    remove comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import LangContext from "@/context/LangContext";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import LanguageDetect from "@/components/language-detect/LanguageDetect";
-//import nextI18NextConfig from '../next-i18next.config.js'
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <LangContext>
