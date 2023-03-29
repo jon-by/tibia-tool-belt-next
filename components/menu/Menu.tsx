649cc23a112c0b715c8b6ab425a2bd446e832e09
Jonas Yure <yure.pereira@codeby.com.br>
2023-03-29 08:56:26 -0300

    testing files

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -1,9 +1,43 @@
-import React, { useState } from "react";
-import { MenuWrapper, MenuContent } from "./menu.styled";
+import React, { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from 'next/router';
+import {
+  MenuWrapper,
+  Content,
+  ImageWrapper,
+  MenuItenM,
+  MenuItens,
+  NavM,
+  NavLinkStyled,
+} from "./menu.styled";
 import MenuToggle from "./MenuToggle";
+
+const menuOptions = [
+  {
+    title: "Home",
+    icon: "/images/home/home.svg",
+    url: "/",
+  },
+  {
+    title: "Loot Spliter",
+    icon: "/images/loot-spliter/loot-spliter.svg",
+    url: "/loot-spliter",
+  },
+  {
+    title: "Itens Timer",
+    icon: "/images/itens-timer/timer-alert-outline.svg",
+    url: "/itens-timer",
+  },
+  {
+    title: "Party Finder",
+    icon: "/images/party-finder/search-player.svg",
+    url: "/party-finder",
+  },
+];
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter()
 
   function handleChange() {
     setIsOpen(!isOpen);
@@ -12,7 +46,55 @@ const Menu = () => {
   return (
     <MenuWrapper>
       <MenuToggle isOpen={isOpen} handleChange={handleChange} />
-      {isOpen && <MenuContent>content</MenuContent>}
+      {isOpen && (
+        <AnimatePresence>
+          {isOpen && (
+            <NavM
+              initial={{ x: -100, opacity: 1 }}
+              animate={{ x: 0 }}
+              transition={{
+                type: "spring",
+                bounce: 0.2,
+                duration: 1,
+                staggerChildren: 0.5,
+              }}
+              exit={{ x: -100, opacity: 0 }}
+            >
+              <MenuItens>
+                {menuOptions.map((menuOption) => {
+                  return (
+                    <MenuItenM
+                      initial={{ x: 200, opacity: 1 }}
+                      animate={{ x: 0 }}
+                      transition={{
+                        type: "spring",
+                        bounce: 0.6,
+                        duration: 1,
+                      }}
+                      exit={{ x: 200, opacity: 0 }}
+                      key={menuOption.url}
+                      whileHover={{
+                        scale: 1.09,
+                        transition: { duration: 0.1 },
+                      }}
+                      whileTap={{ scale: 0.9 }}
+                    >
+                      <NavLinkStyled active={router.pathname === menuOption.url? 1:0} href={menuOption.url}>
+                        <Content>
+                          <ImageWrapper>
+                            <img src={menuOption.icon} alt={menuOption.title} />
+                          </ImageWrapper>
+                          {menuOption.title}
+                        </Content>
+                      </NavLinkStyled>
+                    </MenuItenM>
+                  );
+                })}
+              </MenuItens>
+            </NavM>
+          )}
+        </AnimatePresence>
+      )}
     </MenuWrapper>
   );
 };
--- components/menu/menu.styled.ts
+++ components/menu/menu.styled.ts
@@ -1,12 +1,72 @@
 import styled from "styled-components";
+import Link from 'next/link';
+import { motion } from "framer-motion"
+import { COLORS } from '../../constants/global'
 
-export const MenuWrapper =  styled.div`
+export const MenuWrapper = styled.div`
 position: relative;
+z-index: 10;
 `
 
-export const MenuContent = styled.nav`
-position: absolute;
-top: 100%;
-left: 0;
-background-color: red;
+export const NavM = styled(motion.nav)`   
+background-color: ${COLORS['button-bg']};
+box-shadow: 2px 2px 2px rgba(0,0,0,.3);
+position: absolute;    
+height: auto;
+width: 380px;
+padding: 16px;
+border-radius: 10px;
+
+@media only screen and (max-width: 400px) {
+    width: 340px;
+}
+
+ul{
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: row;
+gap: 16px;
+flex-wrap: wrap;
+}
+`
+
+export const MenuItens = styled.ul` 
+
+    &>div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`
+
+export const MenuItenM = styled(motion.li)`    
+    min-width: 100px;    
+      
+`
+type navLinkStyledProps = {
+    active: number
+}
+export const NavLinkStyled = styled(Link)<navLinkStyledProps>`
+color: ${({active}) => active? COLORS.active: COLORS['white']};
+font-size: 12px;
+padding: 8px;
+
+`
+export const Content = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+`
+export const ImageWrapper = styled.div`
+max-width: 30px;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-bottom: 8px;
+img{
+    width: 100%;
+}
 `
