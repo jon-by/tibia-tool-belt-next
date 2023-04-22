775dfde6f0dd4a3fb4f2c773e201071316834216
jon-by <yure.df@hotmail.com>
2023-04-22 10:23:34 -0300

    refactor: remove unused import

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { ComponentsWrapper } from "@/components/app/app.styled";
 import TopBar from "../top-bar/TopBar";
 
 const Home = () => {
-  return (
-    <ComponentsWrapper>
-        <TopBar/>
-      
-    </ComponentsWrapper>
-  );
+  return <TopBar />;
 };
 
 export default Home;
--- pages/loot-spliter.tsx
+++ pages/loot-spliter.tsx
@@ -1,34 +1,40 @@
-import React from 'react'
+import React from "react";
 import type { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import AnimatedComponent from '@/components/animated-component/AnimatedComponent';
-import Head from 'next/head'
-import LootSpliter from '@/components/loot-spliter/LootSpliter';
+import AnimatedComponent from "@/components/animated-component/AnimatedComponent";
+import Head from "next/head";
+import LootSpliter from "@/components/loot-spliter/LootSpliter";
 
 import { useTranslation } from "next-i18next";
 
 type Props = {};
 const lootSpliter = () => {
-    const { t } = useTranslation("tags");
+  const { t } = useTranslation("tags");
   return (
     <AnimatedComponent>
-        <Head>
+      <Head>
         <title>Loot Spliter | Tibia Tool Belt</title>
-        <meta property="og:title" content="Loot Spliter | Tibia Tool Belt" key="title" />
+        <meta
+          property="og:title"
+          content="Loot Spliter | Tibia Tool Belt"
+          key="title"
+        />
         <meta name="description" content={`${t("loot-spliter")}`} />
-        </Head> 
-        <LootSpliter/>              
+      </Head>
+      <LootSpliter />
     </AnimatedComponent>
-  )
-}
+  );
+};
 
-export default lootSpliter
-
+export default lootSpliter;
 
 export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
-    props: {
-      ...(await serverSideTranslations(locale ?? "pt-BR", ["tags", "loot-spliter", "common"])),
-    },
-  });
-  
+  props: {
+    ...(await serverSideTranslations(locale ?? "pt-BR", [
+      "tags",
+      "loot-spliter",
+      "common",
+    ])),
+  },
+});
