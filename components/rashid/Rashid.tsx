282834684628c099d83af73f9fb0c56476b79cb2
jon-by <yure.df@hotmail.com>
2023-05-07 16:52:03 -0300

    stop using getStaticProps

--- components/rashid/Rashid.tsx
+++ components/rashid/Rashid.tsx
@@ -1,58 +1,38 @@
-
-import { GetStaticProps } from "next";
-
 import Image from "next/image";
 import React from "react";
 import { RashidWrapper } from "./rashid.styled";
 
-
-const getRashidLocation = (day: number, hour: number) => {
-  const rashidPossibleLocations = {
-    0: "Carlin",
-    1: "Svargrond",
-    2: "Liberty Bay",
-    3: "Port Hope",
-    4: "Ankrahmun",
-    5: "Darashia",
-    6: "Edron",
-  };
-  
-  let rashidLocation = "";
-  
-  if (hour > 9) {
-    rashidLocation = rashidPossibleLocations[day as keyof typeof rashidPossibleLocations];
-  } else {
-    const yesterday = day - 1 === -1 ? 6 : day;
-    rashidLocation = rashidPossibleLocations[yesterday as keyof typeof rashidPossibleLocations];
-  }
-  
-  return rashidLocation;
+const rashidPossibleLocations = {
+  0: "Carlin",
+  1: "Svargrond",
+  2: "Liberty Bay",
+  3: "Port Hope",
+  4: "Ankrahmun",
+  5: "Darashia",
+  6: "Edron",
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+type rashidLocationType = keyof typeof rashidPossibleLocations
+
+const Rashid = () => {
+
   var todayBerlin = new Date().toLocaleString("en-US", {
     timeZone: "Europe/Berlin"
   });
-  
+
   const today = new Date(todayBerlin);
-  const day = today.getDay();
-  const hour = today.getHours();
-  
-  const rashidLocation = getRashidLocation(day, hour);
-  
-  return {
-    props: {
-      rashidLocation
-    },
-    revalidate: 60 * 60 // revalidar a cada 1 hora
-  };
-};
+  const day = today.getDay()
+  const hour = today.getHours()
 
-type RashidProps = {
-  rashidLocation: string;
-}
+  let rashidLocation = ""
 
-const Rashid = ({ rashidLocation }: any) => {
+  if (hour > 9) {
+    rashidLocation = rashidPossibleLocations[day as rashidLocationType]
+  } else {
+    const yesterday = day - 1 === -1 ? 6 : day
+    rashidLocation = rashidPossibleLocations[yesterday as rashidLocationType]
+  }
+
   return (
     <RashidWrapper>
       <Image src="/images/Rashid.gif" width={64} height={64} alt="Rashid" />
